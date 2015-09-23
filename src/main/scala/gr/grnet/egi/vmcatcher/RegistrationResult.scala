9838e9b344860f7e580cdc76419cd6bd2dfa5ac6
Christos KK Loverdos <loverdos@gmail.com>
2015-09-23 14:03:44 +0300

    Manually cherry-pick src/main/scala/gr/grnet/egi/vmcatcher/RegistrationResult.scala
    This was introduced in commit e5649a70acce7c843dd8ab06eb5179c595215cf7 but should have been committed earlier.
    Without this file, compilation fails.

---
+++ src/main/scala/gr/grnet/egi/vmcatcher/RegistrationResult.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2014 GRNET S.A.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gr.grnet.egi.vmcatcher
+
+/**
+ *
+ */
+sealed trait RegistrationResult { def isSuccess: Boolean = false }
+
+case object NoTransformerFound extends RegistrationResult
+case object ImageRegistered extends RegistrationResult { override def isSuccess = true }
+case class ImageNotRegistered(exitCode: Int, error: String) extends RegistrationResult
